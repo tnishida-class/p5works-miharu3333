4823ec30f0b3ddafc27eee51b0c8c88be6cb8916
miharu3333 <miharuyoshikawa@gmail.com>
2020-11-17 11:11:15 +0900

    Update sketch.js

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -6,11 +6,17 @@ function setup(){
   x = width / 2;
   y = height / 2;
   vx = 8;
-  vy = 8;
+  vy = 8
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
+  star(x, y, 30, angle);
+  fill(255,255,0)
+  angle += 0.08;
+  x -= -3;
+  y += 3;
   // BLANK[2] (hint: 作った star 関数を使います)
 
   // 端の処理パターン (1) 反対側から出てくる
@@ -23,13 +29,18 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
   }
   endShape(CLOSE);
+}
+
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
 }
 
 function windowResized(){
